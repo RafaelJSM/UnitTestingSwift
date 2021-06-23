c8dbd4b1a4010614a4a4d8047fd32681769d6b8f
Rafael Salazar Magaña <rafael.salazar@dacodes.com.mx>
2021-06-23 13:33:40 -0500

    Initial Commit

---
+++ UnitTestSwiftCourse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTestSwiftCourse
+//
+//  Created by Rafael Salazar on 23/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
